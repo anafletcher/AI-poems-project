6c16fe6716e95110268cdf16759dfe92aa432c9e
anafletcher <153273057+anafletcher@users.noreply.github.com>
2023-12-20 19:16:49 +0000

    Added basic JS form handling and typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "Lightning, thunder, all around",
+    autoStart: true,
+    delay: 30,
+    cursor: null,
+  });
+
+  let poemElement = document.querySelector("#poem");
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
