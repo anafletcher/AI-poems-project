ccd51bfdf69df659c1ce5c4cc69ce4ade6d5702b
anafletcher <153273057+anafletcher@users.noreply.github.com>
2023-12-21 11:30:10 +0000

    Improved the user experience

--- src/index.js
+++ src/index.js
@@ -1,22 +1,27 @@
 function displayPoem(response) {
-  new Typewriter("#poem", {
-    strings: response.data.answer,
-    autoStart: true,
-    delay: 30,
+  let generatedPoem = response.data.answer;
+  let typewriter = new Typewriter("#poem", {
     cursor: null,
+    delay: 30,
   });
+
+  typewriter.typeString(generatedPoem).start();
 }
 
 function generatePoem(event) {
   event.preventDefault();
 
-  let instructionsInput = document.querySelector("#user-instructions");
+  let themeInputElement = document.querySelector("#theme-input");
 
   let apiKey = "f3c5613898a1043cbte4a77d8c1bcfo0";
   let context =
     "You are a poem expert and are great at writing short poems. You must generate a 4-line poem in basic HTML and separate each line with <br/>. do not include a title and sign the poem with '- AI Poem Bot' in <strong> at the end of the poem";
-  let prompt = `Generate a 4-line poem about ${instructionsInput.value}`;
+  let prompt = `Generate a 4-line poem about ${themeInputElement.value}. make sure the poem is 4 lines long`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="pulsing"><em>⏳ Generating a "${themeInputElement.value}" poem for you...</em></div>`;
 
   axios.get(apiUrl).then(displayPoem);
 }
